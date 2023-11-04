96c188b6c01b025d6b049d23e969a9c7d44c95f1
Esteban990821 <esteban.p.990821@gmail.com>
2023-11-04 16:30:46 -0500

    Created new project

---
+++ firmware/readline_simple.d
@@ -0,0 +1,91 @@
+readline_simple.o: readline_simple.c \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdlib.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/Proyectos/VersionPantalla/Blink_litex_pantalla_20/build/software/include/../libc/picolibc.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include/float.h \
+ /home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include/stddef.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/stdlib.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/alloca.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h \
+ /home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include/stdarg.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ctype.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/limits.h \
+ /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/syslimits.h \
+ /home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include-fixed/limits.h \
+ readline.h
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdlib.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/Proyectos/VersionPantalla/Blink_litex_pantalla_20/build/software/include/../libc/picolibc.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+
+/home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include/float.h:
+
+/home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include/stddef.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/stdlib.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/alloca.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h:
+
+/home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include/stdarg.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/ctype.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/limits.h:
+
+/home/esteban/DiscoDuro2/Unal/Digitales_2/litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/syslimits.h:
+
+/home/esteban/miniconda3/envs/fpga/lib/gcc/lm32-elf/9.2.0/include-fixed/limits.h:
+
+readline.h:
