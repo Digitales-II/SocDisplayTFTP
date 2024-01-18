2ce68ea5c43d700dc679d01b87764c970b04801e
Esteban <esteban.p.990821@gmail.com>
2024-01-17 22:55:01 -0500

    Deleted all

--- firmware/boot-helper.d
+++
@@ -1,2 +0,0 @@
-boot-helper.o: \
- /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/litex/litex/soc/cores/cpu/lm32/boot-helper.S
--- firmware/crt0.d
+++
@@ -1,2 +0,0 @@
-crt0.o: \
- /home/esteban/DiscoDuro2/Unal/Digitales_2/litex/litex/litex/soc/cores/cpu/lm32/crt0.S
